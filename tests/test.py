4b79495e11ccebfc7b73b8271fbcf178e52a449b
djamelziane <djamel.ziane@gmx.net>
2023-08-01 16:23:40 +0200

    + table 14

--- tests/test.py
+++ tests/test.py
@@ -9,25 +9,25 @@ def test_metric():
 def test_add():
     assert lib.add(1, 2) == 3
 
-# @pytest.mark.parametrize("test_input,expected", [
-#     (4,8),
-#     (8,12)
-#     ])
-# def test_sum(test_input,expected):
-#     assert lib.add(test_input,4)==expected
-#
-#
-#
-# @pytest.fixture(scope="session")
-# def db_conn():
-#     return mysql.connector.connect(user='root', password='pass',
-#                                   host='127.0.0.1',
-#                                   database='pytesting')
+@pytest.mark.parametrize("test_input,expected", [
+    (4,8),
+    (8,12)
+    ])
+def test_sum(test_input,expected):
+    assert lib.add(test_input,4)==expected
 
-# def test_conn(db_conn):
-#     cursor = db_conn.cursor()
-#     cursor.execute("select aa from pytesting")
-#     record = cursor.fetchone()
-#     assert record[0]==3
+
+
+@pytest.fixture(scope="session")
+def db_conn():
+    return mysql.connector.connect(user='root', password='pass',
+                                  host='127.0.0.1',
+                                  database='test_db')
+
+def test_conn(db_conn):
+    cursor = db_conn.cursor()
+    cursor.execute("select num from test_table")
+    record = cursor.fetchone()
+    assert record[0]==3
 
 
