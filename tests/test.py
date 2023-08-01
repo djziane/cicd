9d3469a9ff40021be52813cbfd22206d746180bb
djamelziane <djamel.ziane@gmx.net>
2023-08-01 09:21:37 +0200

    + Testing mit mark.paramerize

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 from src import lib
-
+import pytest
 
 def test_metric():
     assert lib.metric() == 1.0
@@ -8,3 +8,9 @@ def test_metric():
 def test_add():
     assert lib.add(1, 2) == 3
 
+@pytest.mark.parametrize("test_input,expected", [
+    (4,8),
+    (8,12)
+    ])
+def test_sum(test_input,expected):
+    assert lib.add(test_input,4)==expected
